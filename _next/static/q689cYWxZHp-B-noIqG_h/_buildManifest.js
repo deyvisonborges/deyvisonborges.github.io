c51ba3674a0f36aa0c4ea3eda1583cef246f1090
deyvisonborges <deyvisonsilvaborges@gmail.com>
2022-10-13 12:35:30 -0300

    test new site

---
+++ _next/static/q689cYWxZHp-B-noIqG_h/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5b95e224a6e1a1f8.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
