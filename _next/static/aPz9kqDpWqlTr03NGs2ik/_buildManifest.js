fc0591c73db3a5a53ed169a8f9bb7c072d64740c
deyvisonborges <deyvisonsilvaborges@gmail.com>
2022-10-13 17:20:32 -0300

    add social links

---
+++ _next/static/aPz9kqDpWqlTr03NGs2ik/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/664-490830c05ded00aa.js","static/chunks/pages/index-558ec73d0caf2135.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
